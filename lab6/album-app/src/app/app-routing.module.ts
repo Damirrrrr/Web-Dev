633353832fca066045381b9b431f6177530164cf
Damirrrrr <amanzholovdamir@inbox.ru>
2023-03-08 17:30:06 +0600

    Not Found and routing

--- lab6/album-app/src/app/app-routing.module.ts
+++ lab6/album-app/src/app/app-routing.module.ts
@@ -6,13 +6,17 @@ import { AlbumDetailComponent } from './album-detail/album-detail.component';
 import { AlbumPhotoComponent } from './album-photo/album-photo.component';
 import { AlbumsComponent } from './albums/albums.component';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent },
+export const routes: Routes = [
 { path: '/home', component: HomeComponent },
 { path: '/about', component: AboutComponent },
 { path: '/albums', component: AlbumsComponent },
 { path: '/albums/:id', component: AlbumDetailComponent },
-{ path: '/albums/:id/photos', component: AlbumPhotoComponent },];
+{ path: '/albums/:id/photos', component: AlbumPhotoComponent },
+{ path: '', redirectTo: 'home', pathMatch: 'full'},
+{ path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- lab6/album-app/src/app/app.module.ts
+++ lab6/album-app/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routes } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { AlbumsComponent } from './albums/albums.component';
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
 import { AlbumPhotoComponent } from './album-photo/album-photo.component';
-
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -20,11 +20,13 @@ import { AlbumPhotoComponent } from './album-photo/album-photo.component';
     AlbumsComponent,
     AlbumDetailComponent,
     AlbumPhotoComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
